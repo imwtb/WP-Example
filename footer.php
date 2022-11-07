79d2f270cd5cb43c30626631d2d0854935168997
HEYWTB <heywtb@139.com>
2022-11-07 18:10:31 +0800

    update

--- footer.php
+++ footer.php
@@ -8,18 +8,14 @@
   <?php
   if (has_nav_menu('secondary')) {
     wp_nav_menu([
-      'container'            => 'nav',
-      'container_class'      => 'bottomnav',
-      'theme_location'       => 'secondary',
+      'container'       => 'nav',
+      'container_class' => 'bottomnav',
+      'theme_location'  => 'secondary',
     ]);
   }
   ?>
 
-  Copyright &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
-
-  <a href="<?php echo esc_url('https://beian.miit.gov.cn/'); ?>"><?php esc_html_e('京ICP备2022018789号'); ?></a>
-
-  <?php the_privacy_policy_link(); ?>
+  Copyright &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> <a href="<?php echo esc_url('https://beian.miit.gov.cn/'); ?>"><?php esc_html_e('京ICP备2022018789号'); ?></a> <?php the_privacy_policy_link(); ?>
 </footer>
 
 <?php wp_footer(); ?>
