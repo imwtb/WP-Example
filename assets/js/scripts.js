bcc407e6dd5fd34e8fa129b6c6f5a366c9043a7a
HEYWTB <heywtb@139.com>
2022-11-10 23:56:54 +0800

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,7 +8,7 @@
   if (sidebar.length) {
     sidebar.stickySidebar({
       containerSelector: '.layout',
-      topSpacing: 32,
+      topSpacing: 96,
       bottomSpacing: 32,
       resizeSensor: true,
       minWidth: 1200,
