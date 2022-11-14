27784e5948571509d942d9e9e37753b8b2f938b5
HEYWTB <heywtb@139.com>
2022-11-14 22:01:29 +0800

    Update author.php

--- author.php
+++ author.php
@@ -5,21 +5,7 @@
     <?php
     the_archive_title('<h2>', '</h2>');
 
-<<<<<<< HEAD
-    $user_id = get_the_author_meta('ID');
-    echo get_avatar($user_id) . '<br>';
-    echo __('ID：', 'imwtb') . get_the_author_meta('ID') . '<br>';
-    echo __('用户名：', 'imwtb') . get_the_author_meta('user_login', $user_id) . '<br>';
-=======
-    if (is_author()) {
-      global $post;
-      $user_id   = $post->post_author;
-      $user_role = get_userdata($user_id)->roles[0];
-    } else {
-      $user_id   = get_current_user_id();
-      $user_role = wp_get_current_user()->roles[0];
-    }
-
+    $user_id   = get_the_author_meta('ID');
     $user_name = get_the_author_meta('display_name', $user_id) ? get_the_author_meta('display_name', $user_id) : __('游客', 'news-text');
     switch ($user_role) {
       case 'administrator':
@@ -41,11 +27,11 @@
         $user_purview = __('游客', 'news-text');
         break;
     }
+
     echo get_avatar($user_id) . '<br>';
-    echo __('ID：', 'imwtb') . get_the_author_meta('ID', $user_id) . '<br>';
+    echo __('ID：', 'imwtb') . $user_id . '<br>';
     echo __('用户名：', 'imwtb') . $user_purview . '<br>';
     echo __('权限：', 'imwtb') . get_the_author_meta('user_login', $user_id) . '<br>';
->>>>>>> 5633d27b3ef6dc7fc3a5f9c61ed448b9f4fd4f4d
     echo __('注册时间：', 'imwtb') . get_the_author_meta('user_registered', $user_id) . '<br>';
     echo __('等级：', 'imwtb') . get_the_author_meta('user_level', $user_id) . '<br>';
     echo __('名字：', 'imwtb') . get_the_author_meta('first_name', $user_id) . '<br>';
@@ -55,11 +41,8 @@
     echo __('邮箱：', 'imwtb') . get_the_author_meta('user_email', $user_id) . '<br>';
     echo __('网址：', 'imwtb') . get_the_author_meta('user_url', $user_id) . '<br>';
     echo __('简介：', 'imwtb') . get_the_author_meta('description', $user_id) . '<br>';
-<<<<<<< HEAD
-=======
     echo __('文章数量：', 'imwtb') . count_user_posts($user_id) . '<br>';
     echo __('评论数量：', 'imwtb') . count(get_comments($user_id)) . '<br>';
->>>>>>> 5633d27b3ef6dc7fc3a5f9c61ed448b9f4fd4f4d
 
     $main_query = new WP_Query([
       'fields'              => 'ids',
